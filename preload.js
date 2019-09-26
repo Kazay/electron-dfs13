61ded566b4d20fa3b54b729f468225d628b912bd
Kevin Czaja <czajakevin@gmail.com>
2019-09-26 10:18:45 +0200

    switching

--- preload.js
+++ preload.js
@@ -1,3 +1,7 @@
+var fs = require('fs')
+  , gm = require('gm').subClass({imageMagick: '7+'});
+const { remote } = require('electron');
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -10,3 +14,15 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 })
+
+const run = () => {
+	const src = remote.app.getAppPath()+'/test.jpg';
+	const dst = remote.app.getAppPath()+'/test-thumbnail.png';
+	console.log(src, dst);
+	gm(src).crop(240, 240).write(dst, (err) => {
+ 		if (!err) console.log('done');
+		else console.log(err);
+	});
+}
+
+window.addEventListener('DOMContentLoaded', run);
